d59843cdd75a51d4171404d3a2453b3e3192863a
unknown <adarshrg1998@gmail.com>
2023-08-19 16:10:45 +0530

    first commit

---
+++ src/components/BabylonExamples.vue
@@ -0,0 +1,28 @@
+<template>
+ <div>
+  <h3>Babylon Examples</h3>
+  <canvas></canvas>
+ </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { BasicScene } from "@/BabylonExamples/BasicScene";
+
+
+export default defineComponent({
+  name: 'BabylonExamples',
+ mounted(){
+const canvas=document.querySelector("canvas")!
+new BasicScene(canvas)
+ }
+});
+</script>
+
+
+<style scoped>
+canvas{
+  width:100%;
+  height:80%;
+}
+</style>
